38a5c67d5b757222c6c1b3f71aa068a55ee97c53
Greg Meyer <greg.meyer@datadoghq.com>
2018-12-06 13:59:30 -0500

    testing

--- ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
+++ ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
@@ -81,16 +81,18 @@ class IbmMqCheck(AgentCheck):
         """
         for mname, pymqi_value in iteritems(metrics.queue_metrics()):
             try:
-                m = queue.inquire(pymqi_value)
                 mname = '{}.queue.{}'.format(self.METRIC_PREFIX, mname)
+                log.info('collecting: {}'.format(mname))
+                m = queue.inquire(pymqi_value)
                 self.gauge(mname, m, tags=tags)
             except pymqi.Error as e:
                 self.warning("Error getting queue stats: {}".format(e))
 
         for mname, func in iteritems(metrics.queue_metrics_functions()):
             try:
-                m = func(queue)
                 mname = '{}.queue.{}'.format(self.METRIC_PREFIX, mname)
+                log.info('collecting: {}'.format(mname))
+                m = func(queue)
                 self.gauge(mname, m, tags=tags)
             except pymqi.Error as e:
                 self.warning("Error getting queue stats: {}".format(e))
